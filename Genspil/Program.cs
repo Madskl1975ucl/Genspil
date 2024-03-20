5a01ba13efd0afb6a96ce5d5949432960fc06d51
Kenneth Haagen Sørensen <139569773+KenHSo@users.noreply.github.com>
2024-03-20 17:20:19 +0100

    Added Methods AddGame, WriteFile, Show and Delete

---
+++ Genspil/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Genspil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BoardGame game = new();
+
+            //game.AddGame();
+            //game.AddGame();
+
+
+            //game.WriteFile();
+
+            //game.Show();
+
+            //game.ReadFile();
+
+            game.Delete();
+
+            Console.ReadKey();
+        }
+    }
+}
